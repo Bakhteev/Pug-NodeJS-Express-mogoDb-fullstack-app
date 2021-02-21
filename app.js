841f14bd8d4f1f92eb5a46238aefaf5ddb528f71
Bogdan-create <bogdan-shigaev@mail.ru>
2021-02-21 19:57:28 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.set('view engine', 'pug')
+// app.set('views', path.join(__dirname, '/views'))
+app.listen(4200)
+app.use(express.static('public'))
+
+// app.use(express.urlencoded())
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => res.status(200).render('index'))
+
+app.get('/about', (req, res) => res.status(200).render('about'))
+
+app.use((req, res) => res.status(404).render('404'))
+
+console.log('heloo world')
---
+++ public/js/slider.js
@@ -0,0 +1,35 @@
+let viewport = document.querySelector('.slider-viewport').offsetWidth
+let slider = document.querySelector('.slider')
+let btnNext = document.querySelector('.slider-btn-next')
+let btnPrev = document.querySelector('.slider-btn-prev')
+let slides = document.querySelectorAll('.slider-slide')
+let sliderCurrent = document.querySelector('.slider-current')
+let sliderNumber = document.querySelector('.slider-number')
+let viewSlide = 0
+sliderCurrent.innerHTML = 1
+sliderNumber.innerHTML = slides.length
+btnNext.addEventListener('click', function () {
+  if (viewSlide < slides.length - 1) {
+    viewSlide++
+    sliderCurrent.innerHTML = viewSlide + 1
+    slider.style.transform = `translateX(${-viewSlide * viewport}px)`
+    return viewSlide
+  } else {
+    viewSlide = slides.length - 1
+    return viewSlide
+  }
+})
+btnPrev.addEventListener('click', function () {
+  if (viewSlide > 0 || viewSlide == slides.length - 1) {
+    viewSlide--
+    slider.style.transform = `translateX(${-viewSlide * viewport}px)`
+    sliderCurrent.innerHTML = viewSlide + 1
+    return viewSlide
+  } else {
+    viewSlide = 0
+    sliderCurrent.innerHTML = viewSlide + 1
+    return viewSlide
+  }
+})
+
+
